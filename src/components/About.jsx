1970824ea4f41862f9747e9a63b2c7d922495b00
ishtiaqSamdani <82057297+ishtiaqSamdani@users.noreply.github.com>
2022-11-29 15:07:34 +0530

    2

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <h1>Yes this is...</h1>
+    </>
+  );
+}
+
+export default About;
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+//add base in vite
+
+
 export default defineConfig({
+  base: '/ReactWithVite/',
   plugins: [react()]
 })
