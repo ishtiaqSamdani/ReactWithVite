60c6709ad966f3f8e231766dc399e40484c9f2ce
ishtiaqSamdani <82057297+ishtiaqSamdani@users.noreply.github.com>
2022-11-29 15:17:59 +0530

    url again

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,20 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link className="link" to="/">
+            <Link className="link" to="/ReactWithVite">
               Home
             </Link>
           </li>
           <li>
-            <Link className="link" to="/about">
+            <Link className="link" to="/ReactWithVite/about">
               About
             </Link>
           </li>
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/ReactWithVite" element={<Home />} />
+        <Route path="/ReactWithVite/about" element={<About />} />
       </Routes>
     </>
   );
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  // base: '/ReactWithVite/',
+  base: '/ReactWithVite/',
   plugins: [react()]
 })
